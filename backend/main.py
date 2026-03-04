d752f90c15a2c8720025a55c94c107ad1d682c97
Arturo Alvarez <arturo@alvarez.com>
2026-03-04 20:52:15 -0300

    feat: 58 plataformas de Sherlock

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 import json
 import os
-import sys
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
@@ -20,23 +19,30 @@ def get_sites(username):
         'LinkedIn': f'https://www.linkedin.com/in/{username}',
         'Snapchat': f'https://www.snapchat.com/add/{username}',
         'Pinterest': f'https://www.pinterest.com/{username}/',
-        # Video
+        'Bluesky': f'https://bsky.app/profile/{username}.bsky.social',
+        'Mastodon': f'https://mastodon.social/@{username}',
+        'Tumblr': f'https://{username}.tumblr.com',
+        'Threads': f'https://www.threads.net/@{username}',
+        'Discord': f'https://discord.com',
+        
+        # Video/Streaming
         'YouTube': f'https://www.youtube.com/@{username}',
         'Twitch': f'https://www.twitch.tv/{username}',
         'Vimeo': f'https://vimeo.com/{username}',
+        'DailyMotion': f'https://www.dailymotion.com/{username}',
         'Odysee': f'https://odysee.com/@{username}',
+        
         # Gaming
         'Steam': f'https://steamcommunity.com/id/{username}',
-        'Xbox Live': f'https://xboxgamertag.com/search/{username}',
         'PlayStation': f'https://psnprofiles.com/{username}',
         'Roblox': f'https://www.roblox.com/user.aspx?username={username}',
         'Minecraft': f'https://namemc.com/profile/{username}',
         'osu!': f'https://osu.ppy.sh/users/{username}',
         'Chess.com': f'https://www.chess.com/member/{username}',
         'Lichess': f'https://lichess.org/@/{username}',
-        # Forums
+        
+        # Forums/Community
         'Reddit': f'https://www.reddit.com/user/{username}',
-        'Discord': f'https://discord.com/users/{username}',
         'Stack Overflow': f'https://stackoverflow.com/users/{username}',
         'GitLab': f'https://gitlab.com/{username}',
         'Bitbucket': f'https://bitbucket.org/{username}/',
@@ -44,56 +50,64 @@ def get_sites(username):
         'Dev.to': f'https://dev.to/{username}',
         'Medium': f'https://medium.com/@{username}',
         'Vercel': f'https://vercel.com/{username}',
-        'Netlify': f'https://app.netlify.com/teams/{username}',
+        
         # Music
         'Spotify': f'https://open.spotify.com/user/{username}',
         'SoundCloud': f'https://soundcloud.com/{username}',
         'Last.fm': f'https://www.last.fm/user/{username}',
-        'Bandcamp': f'https://{username}.bandcamp.com',
+        'Bandcamp': f'https://www.bandcamp.com/{username}',
+        
         # Professional
         'Behance': f'https://www.behance.net/{username}',
         'Dribbble': f'https://dribbble.com/{username}',
         'Figma': f'https://www.figma.com/@{username}',
         'Product Hunt': f'https://www.producthunt.com/@{username}',
-        # Photography
+        
+        # Photography/Art
         'Flickr': f'https://www.flickr.com/people/{username}/',
         'Unsplash': f'https://unsplash.com/@{username}',
         '500px': f'https://500px.com/p/{username}',
         'DeviantArt': f'https://www.deviantart.com/{username}',
         'ArtStation': f'https://www.artstation.com/{username}',
-        # Dev Packages
+        
+        # Development
         'Docker Hub': f'https://hub.docker.com/u/{username}/',
-        'NPM': f'https://www.npmjs.com/~{username}',
         'PyPI': f'https://pypi.org/user/{username}/',
+        'NPM': f'https://www.npmjs.com/~{username}',
         'RubyGems': f'https://rubygems.org/profiles/{username}',
-        # Blogs
+        'GitHub Gist': f'https://gist.github.com/{username}',
+        
+        # Writing/Blogs
         'Quora': f'https://www.quora.com/profile/{username}',
         'Substack': f'https://{username}.substack.com',
         'Hashnode': f'https://hashnode.com/@{username}',
-        # Crypto
+        
+        # Crypto/Web3
         'OpenSea': f'https://opensea.io/{username}',
+        
+        # Other
         'Linktree': f'https://linktr.ee/{username}',
-        'OnlyFans': f'https://onlyfans.com/{username}',
         'Patreon': f'https://www.patreon.com/{username}',
-        'Tumblr': f'https://{username}.tumblr.com',
-        'Mastodon': f'https://mastodon.social/@{username}',
-        'Bluesky': f'https://bsky.app/profile/{username}.bsky.social',
-        'Threads': f'https://www.threads.net/@{username}',
+        'Buy Me a Coffee': f'https://www.buymeacoffee.com/{username}',
+        'Ko-fi': f'https://ko-fi.com/{username}',
+        'OnlyFans': f'https://onlyfans.com/{username}',
     }
 
 @app.route('/search', methods=['POST'])
 def search_username():
     data = request.get_json()
     username = data.get('username', '').strip()
+    
     if not username:
         return jsonify({'error': 'Username requerido'}), 400
     
     sites = get_sites(username)
     results = []
+    found_count = 0
     
     def check_site(site_name, url):
         try:
-            response = requests.head(url, allow_redirects=True, timeout=5)
+            response = requests.head(url, allow_redirects=True, timeout=5, headers={'User-Agent': 'Mozilla/5.0 (compatible; Sherlock/0.16.0)'})
             exists = response.status_code == 200
             return {
                 'platform': site_name,
@@ -109,17 +123,21 @@ def search_username():
                 'username': username
             }
     
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    # Parallel processing with limited workers
+    with ThreadPoolExecutor(max_workers=20) as executor:
         futures = {executor.submit(check_site, name, url): name 
                   for name, url in sites.items()}
         for future in as_completed(futures):
-            results.append(future.result())
+            result = future.result()
+            results.append(result)
+            if result['exists']:
+                found_count += 1
     
     return jsonify({
         'username': username,
         'results': results,
         'count': len(results),
-        'found_count': len([r for r in results if r['exists']])
+        'found_count': found_count
     })
 
 @app.route('/health', methods=['GET'])
